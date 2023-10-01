93f164759b0f2d199336ed9cade2685ef9da2861
Paulius <40724700+jungassuo@users.noreply.github.com>
2023-10-01 03:34:09 +0300

    send async data when new connection is established

--- SignalRSample/Hubs/UserHub.cs
+++ SignalRSample/Hubs/UserHub.cs
@@ -6,6 +6,22 @@ namespace SignalRSample.Hubs
     {
         public static int TotalViews { get; set; } = 0;
 
+        public static int TotalUsers { get; set; } = 0;
+
+        public override Task OnConnectedAsync()
+        {
+            TotalUsers++;
+            Clients.All.SendAsync("updateTotalUsers", TotalUsers).GetAwaiter().GetResult();
+            return base.OnConnectedAsync();
+        }
+
+        public override Task OnDisconnectedAsync(Exception? exception)
+        {
+            TotalUsers--;
+            Clients.All.SendAsync("updateTotalUsers", TotalUsers).GetAwaiter().GetResult();
+            return base.OnDisconnectedAsync(exception);
+        }
+
         public async Task NewWindowLoaded()
         { 
             TotalViews++;
---
+++ SignalRSample/wwwroot/js/usersCount.js
@@ -0,0 +1,35 @@
+﻿//create connection with signalR
+var connectionUserCount = new signalR.HubConnectionBuilder().withUrl("/hubs/userCount").build();
+
+//connect to methods that hub invokes
+connectionUserCount.on("updateTotalViews", (value) => {
+    var newCountSpan = document.getElementById("totalViewsCounter");
+    newCountSpan.innerText = value.toString();
+});
+
+connectionUserCount.on("updateTotalUsers", (value) => {
+    var newCountSpan = document.getElementById("totalUsersCounter");
+    newCountSpan.innerText = value.toString();
+});
+
+//invoke hub methods
+function newWindowLoadedOnClient() {
+    connectionUserCount.send("NewWindowLoaded");
+}
+
+//start connection
+
+function fulfilled()
+{
+    console.log("Connection successful");
+    newWindowLoadedOnClient();
+}
+
+function rejected()
+{
+
+}
+connectionUserCount.start().then(fulfilled, rejected);
+
+
+
